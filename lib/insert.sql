d2846a11e1c9222be39f7ebdf1dc7368490b60f9
Denzel <jdenzelmab@gmail.com>
2023-08-14 19:26:36 -0400

    edited boolean values

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,9 +1,9 @@
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 4, 'M', 'black', 'wack', TRUE);
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 4, 'M', 'black', 'wack', TRUE);
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 4, 'W', 'black', 'wack', TRUE);
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 4, 'M', 'black', 'wack', TRUE);
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 4, 'W', 'black', 'wack', TRUE);
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 4, 'M', 'black', 'wack', TRUE);
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 4, 'W', 'black', 'wack', TRUE);
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL , 4, 'W', 'black', 'wack', TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 4, 'M', 'black', 'wack', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 4, 'M', 'black', 'wack', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 4, 'W', 'black', 'wack', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 4, 'M', 'black', 'wack', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 4, 'W', 'black', 'wack', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 4, 'M', 'black', 'wack', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 4, 'W', 'black', 'wack', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL , 4, 'W', 'black', 'wack', 1);
 
